cfa62782d3f9f0af1eb3719d2c90f998c13830d4
laetuz <laetuzg@gmail.com>
2023-12-18 05:18:42 +0700

    refactored some stuff and added some functions related to the UI.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,23 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     //Making arraays
-    let emojis = ["💀", "🗿", "🦈", "👽"]
+    let emojis = ["💀", "🗿", "🦈", "👽", "💀", "🗿", "🦈", "👽"]
+    @State var cardCounts = 4
     
     var body: some View {
-        HStack {
-            //Making ForEach
-            ForEach(0..<4, id: \.self){ num in
-                CardViewCustom(content: emojis[num])
-            }
-            
-        }.foregroundColor(.blue).padding()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                cards()
+            }
+            Spacer()
+            HStack {
+                if cardCounts > 0 {
+                    Button(action: decreaseCardCount, label: {
+                        Image(systemName: "minus")
+                    })
+                    Spacer()
+                    moreButton(title: "plus")
+                } else {
+                    moreButton(title: "plus.circle").font(.largeTitle)
+                }
+            }
         }
         .padding()
+    }
+    
+    private func cards() -> some View {
+        HStack {
+            //Making ForEach
+            LazyVGrid(
+                //columns: [GridItem(), GridItem(), GridItem()]
+                columns: [GridItem(.adaptive(minimum: 120))]
+            ) {
+                ForEach(0..<cardCounts, id: \.self){ num in
+                    CardViewCustom(
+                        content: emojis[num])
+                    .font(.largeTitle)
+                    .aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                }
+            }
+        }.foregroundColor(.blue).padding()
+    }
+    
+    private func decreaseCardCount() {
+        cardCounts = max(cardCounts - 1, 0)
+    }
+
+    private func increaseCardCount() {
+        cardCounts = min(cardCounts + 1, emojis.count)
+    }
+    
+    private func moreButton(title: String) -> some View{
+        Button(action: increaseCardCount, label: {
+            Image(systemName: title)
+        })
     }
 }
 
@@ -36,6 +72,7 @@ struct CardViewCustom: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
+            /****used to be like this:*
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
@@ -43,6 +80,15 @@ struct CardViewCustom: View {
             } else {
                 base
             }
+             */
+            
+        //The new approach/best approach is to add group (a ForEach of one)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content)
+            }//.opacity(isFaceUp ? 1: 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             //toggle is the same as isFaceUp = !isFaceUp
